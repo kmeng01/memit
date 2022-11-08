5279284229280623929ebbfb0bbe0c552e87630b
Kevin Meng <kevin.meng.01@gmail.com>
2022-11-08 17:58:56 -0500

    Update deprecated wiki texts

--- experiments/causal_trace.py
+++ experiments/causal_trace.py
@@ -665,7 +665,7 @@ def get_embedding_cov(mt):
         ds_name = "wikitext"
         raw_ds = load_dataset(
             ds_name,
-            dict(wikitext="wikitext-103-raw-v1", wikipedia="20200501.en")[ds_name],
+            dict(wikitext="wikitext-103-raw-v1", wikipedia="20220301.en")[ds_name],
         )
         try:
             maxlen = model.config.n_positions
--- rome/layer_stats.py
+++ rome/layer_stats.py
@@ -96,7 +96,7 @@ def layer_stats(
     def get_ds():
         raw_ds = load_dataset(
             ds_name,
-            dict(wikitext="wikitext-103-raw-v1", wikipedia="20200501.en")[ds_name],
+            dict(wikitext="wikitext-103-raw-v1", wikipedia="20220301.en")[ds_name],
         )
         maxlen = model.config.n_positions
         if batch_tokens is not None and batch_tokens < maxlen:
